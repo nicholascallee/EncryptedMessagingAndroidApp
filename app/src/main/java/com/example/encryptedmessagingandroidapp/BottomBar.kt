8353996e154757121c4859fb6efe345c30436a5f
nicka <nickallee8529@gmail.com>
2023-08-04 15:16:39 -0500

    moved title bar to seperate pg

--- app/src/main/java/com/example/encryptedmessagingandroidapp/BottomBar.kt
+++ app/src/main/java/com/example/encryptedmessagingandroidapp/BottomBar.kt
@@ -3,14 +3,36 @@ package com.example.encryptedmessagingandroidapp
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.encryptedmessagingandroidapp.data.BottomNavigationScreens
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun customTopBar(){
+        TopAppBar(
+            title = {
+                androidx.compose.material.Text(text = "Nicholas Christopher Allee's RSA Messager.")
+            },
+            navigationIcon = {
+                // Custom icon for navigation
+                androidx.compose.material3.Icon(
+                    imageVector = Icons.Filled.Menu,
+                    contentDescription = null,
+                    tint = Color.White
+                )
+            }
+        )
+}
 @Composable
 fun customBottomBar(navController: NavController, items: List<BottomNavigationScreens>){
         BottomNavigation {
--- app/src/main/java/com/example/encryptedmessagingandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/encryptedmessagingandroidapp/MainActivity.kt
@@ -7,23 +7,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.example.encryptedmessagingandroidapp.ui.theme.EncryptedMessagingAndroidAppTheme
 import com.example.encryptedmessagingandroidapp.data.BottomNavigationScreens
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.Icon
-import androidx.compose.ui.graphics.Color
 
 
 
@@ -49,30 +42,17 @@ class MainActivity : ComponentActivity() {
 
                     Scaffold(
                         topBar = {
-                            TopAppBar(
-                                title = {
-                                    Text(text = "test123")
-                                },
-                                navigationIcon = {
-                                    // Custom icon for navigation
-                                    Icon(
-                                        imageVector = Icons.Filled.Menu,
-                                        contentDescription = null,
-                                        tint = Color.White
-                                    )
-                                }
-                            )
+                            customTopBar()
                         },
                         bottomBar = {
                             customBottomBar(navController = navController, items = bottomNavigationItems )
-                        },
-
+                        }
                     )
                     { innerPadding ->
                         // Add padding to your content
                         Column(
                             Modifier.padding(innerPadding)
-                        ) {
+                        ){
                             customNavHost(navController = navController)
                         }
                     }
